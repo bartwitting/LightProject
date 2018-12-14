c906e38592e8bcdfa53de102af4d3a65e4439e1d
bartwitting <bartwitting@icloud.com>
2018-12-14 11:15:20 +0100

    Comments

--- LightProject/ViewController.swift
+++ LightProject/ViewController.swift
@@ -9,20 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    /// Defining the variable of the state of the background
     var lightOn = true
+    
+    /// Building the app
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    /// Action to change the state of the background
     @IBAction func buttonPressed(_ sender: UIButton) {
         lightOn = !lightOn
         updateUI()
     }
     
-    
+    /// Function to change the stat of the background
     func updateUI() {
         view.backgroundColor = lightOn ? .white : .black
     }
